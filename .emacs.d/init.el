6a52a261ff3503cc58ed1b136ebc05ad2ce5bd60
kosugi <kosugi.takuya+github@gmail.com>
2014-04-07 10:01:55 +0900

    T-code for emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,12 @@
 ; common key bindings
 (global-set-key "\C-h" 'backward-delete-char)
+(global-set-key "\C-o" 'toggle-input-method)
+
+; Package
+(require 'package)
+(package-initialize)
+
+; T-code
+(require 'tc-setup)
+(setq tcode-help-with-real-keys t)
 
