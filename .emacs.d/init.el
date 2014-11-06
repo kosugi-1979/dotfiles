ae07203862ee68a17316980771bbf18a1e5155db
kosugi <kosugi.takuya+github@gmail.com>
2014-11-06 15:13:30 +0900

    use melpa during emacs package installation.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,6 +7,11 @@
 
 ; Package
 (require 'package)
+(add-to-list 'package-archives
+ '("melpa" . "http://melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+ ;; For important compatibility libraries like cl-lib
+ (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
 
 ; T-code
